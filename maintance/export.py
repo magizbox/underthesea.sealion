f2eafc12e20b41ac08f0840f67617725fd911785
rain <brother.rain.1024@gmail.com>
2017-11-15 14:14:38 +0700

    add maintance scripts

--- maintance/export.py
+++ maintance/export.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     service_api = "http://localhost:8000"
     data_folder = join(dirname(__file__), "data")
     exporter = DialogueDocumentExport(service_api)
-    exporter.export({"act": "true"}, join(data_folder, "acts_20171101.json"))
-    exporter.export({"category": "true"},
-                    join(data_folder, "categories_20171101.json"))
+    exporter.export({"act": "true"}, join(data_folder, "acts_20171114.json"))
+    # exporter.export({"category": "true"},
+    #                 join(data_folder, "categories_20171101.json"))

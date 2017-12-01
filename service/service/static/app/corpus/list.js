9334d654ff21a81bb4cbe65a86900aa6f03abed3
Nguyen Thi Hau <haunt@gmail.com>
2017-12-01 15:36:55 +0700

    update task corpus

--- service/service/static/app/corpus/list.js
+++ service/service/static/app/corpus/list.js
@@ -1,64 +1,74 @@
 app.controller("ListCorpusCtrl", function ($scope, Corpus, STATUSES, QUALITIES, $stateParams, $state) {
-    $scope.STATUSES = STATUSES;
-    $scope.QUALITIES = QUALITIES;
-    $scope.status = $stateParams.status ? $stateParams.status : 'ALL';
-    $scope.quality = $stateParams.quality ? $stateParams.quality : 'ALL';
+  $scope.STATUSES = STATUSES;
+  $scope.QUALITIES = QUALITIES;
+  $scope.status = $stateParams.status ? $stateParams.status : 'ALL';
+  $scope.quality = $stateParams.quality ? $stateParams.quality : 'ALL';
 
-    $scope.listTask = [
-        {
-            name: 'Word Segmentation',
-            value: 'WS'
-        },
-        {
-            name: 'POS Tagging',
-            value: 'PO'
-        },
-        {
-            name: 'Chunking',
-            value: 'CH'
-        },
-        {
-            name: 'Named Entity Recognition',
-            value: 'NE'
-        },
-        {
-            name: 'Dialog Acts',
-            value: 'DA'
-        },
-        {
-            name: 'Category',
-            value: 'CA'
-        },
-        {
-            name: 'Sentiment',
-            value: 'SA'
-        }
+  $scope.listTask = [
+    {
+      name: 'Word Segmentation',
+      value: 'WS'
+    },
+    {
+      name: 'POS Tagging',
+      value: 'PO'
+    },
+    {
+      name: 'Chunking',
+      value: 'CH'
+    },
+    {
+      name: 'Named Entity Recognition',
+      value: 'NE'
+    },
+    {
+      name: 'Dialog Acts',
+      value: 'DA'
+    },
+    {
+      name: 'Category',
+      value: 'CA'
+    },
+    {
+      name: 'Sentiment',
+      value: 'SA'
+    }
 
-    ];
-    var query = {};
-    $scope.selectedTask = {};
-    if ($scope.status != "ALL") {
-        query["status"] = $scope.status;
-    }
-    if ($scope.quality != "ALL") {
-        query["quality"] = $scope.quality;
-    }
+  ];
+  var query = {};
+  $scope.selectedTask = {};
+  if ($scope.status != "ALL") {
+    query["status"] = $scope.status;
+  }
+  if ($scope.quality != "ALL") {
+    query["quality"] = $scope.quality;
+  }
+  $scope.getListCorpus = function () {
     Corpus.query(query).then(function (data) {
-        $scope.corpora = data;
+      $scope.corpora = data;
     });
-    $scope.delete = function (corpusId) {
-        Corpus.delete({id: corpusId}).$promise.then(function () {
-            $state.reload();
-        })
-    };
-    $scope.openEdit = function(corpus){
-        $scope.tmp = $.extend({}, corpus);
-    };
+  };
 
-    $scope.updateCorpus = function(){
-        console.log($scope.selectedTask);
-        Corpus.update({"id": $scope.tmp.id}, $scope.tmp).$promise.then(function(){
-            window.location.reload();
-        });
-    };
+  $scope.getListCorpus();
+
+  $scope.delete = function (corpusId) {
+    Corpus.delete({id: corpusId}).$promise.then(function () {
+      $state.reload();
+    })
+  };
+  $scope.openEdit = function (corpus) {
+    $scope.tmp = $.extend({}, corpus);
+  };
+
+  $scope.updateCorpus = function () {
+    var list = _.chain($scope.selectedTask)
+      .pick(function (value, key, object) {
+        return value == true;
+      })
+      .allKeys().value();
+    $scope.tmp["tasks"] = list.toString();
+    Corpus.update({"id": $scope.tmp.id}, $scope.tmp).$promise.then(function () {
+      $scope.getListCorpus();
+    });
+  };
 });

cf717a86259dc9a68828e801f134b84c29fee9df
Nguyen Thi Hau <haunt@gmail.com>
2017-12-02 12:16:00 +0700

    edit documents list controler

--- service/service/static/app/corpus/detail.js
+++ service/service/static/app/corpus/detail.js
@@ -1,47 +1,59 @@
 app.controller("DetailCorpusCtrl", function ($scope, $stateParams, Corpus, $state, STATUSES, QUALITIES, Document, Params, $filter) {
-  $scope.id = $stateParams.id;
-  var params = JSON.parse(JSON.stringify($stateParams));
-  params["corpus"] = params["id"];
-  $scope.params = Params(params, {
-    "offset": 0,
-    "limit": 10,
-    "corpus": 1,
-    "status": null,
-    "quality": null,
-    "search": null,
-    "act": null,
-    "category": null,
-    "sentiment": null
-  });
-  $scope.statuses = STATUSES;
+  $scope.init = function () {
+    $scope.id = $stateParams.id;
+    var params = JSON.parse(JSON.stringify($stateParams));
+    params["corpus"] = params["id"];
+    $scope.params = Params(params, {
+      "offset": 0,
+      "limit": 10,
+      "corpus": 1,
+      "status": null,
+      "quality": null,
+      "search": null,
+      "act": null,
+      "category": null,
+      "sentiment": null
+    });
+    $scope.statuses = STATUSES;
+    $scope.quality = null;
+    $scope.qualities = QUALITIES;
+
+    $scope.getInfoCorpus();
+    $scope.getListDocument();
+  };
+
 
   $scope.showStatus = function () {
     var selected = $filter('filter')($scope.statuses,
       {value: $scope.params.status});
     return ($scope.params.status && selected.length) ? selected[0].text : 'All';
   };
-  $scope.quality = null;
-  $scope.qualities = QUALITIES;
+
 
   $scope.showQuality = function () {
     var selected = $filter('filter')($scope.qualities,
       {value: $scope.params.quality});
     return ($scope.params.quality && selected.length) ? selected[0].text : 'All';
   };
-  if ($scope.id) {
+
+  $scope.getInfoCorpus = function () {
     Corpus.get({id: $scope.id}, function (corpus) {
+      corpus.tasks = corpus.tasks.split(",");
       $scope.corpus = corpus;
     });
-  }
+  };
 
-  Document.query($scope.params).then(function (documents) {
-    $scope.documents = documents;
-  });
-  Document.pagination($scope.params).then(function (result) {
-    $scope.totalItems = result["totalItems"];
-    $scope.itemsPerPage = result["itemsPerPage"];
-    $scope.currentPage = result["currentPage"];
-  });
+  $scope.getListDocument = function () {
+    Document.query($scope.params).then(function (documents) {
+      $scope.documents = documents;
+    });
+    Document.pagination($scope.params).then(function (result) {
+      $scope.totalItems = result["totalItems"];
+      $scope.itemsPerPage = result["itemsPerPage"];
+      $scope.currentPage = result["currentPage"];
+    });
+  };
+
 
   $scope.update = function () {
     return Corpus.update({id: $scope.id}, $scope.corpus);
@@ -61,26 +73,13 @@ app.controller("DetailCorpusCtrl", function ($scope, $stateParams, Corpus, $stat
   $scope.filterChanged = function () {
     $state.go(".", $scope.params);
   };
-  $scope.tasks = [
-    {
-      "name": "act",
-      "label": "DA"
-    },
-    {
-      "name": "category",
-      "label": "CA"
-    },
-    {
-      "name": "sentiment",
-      "label": "SA"
-    },
-  ];
+
   $scope.toggle = function (task) {
     var states = ["true", "false", null];
-    var i = states.indexOf($scope.params[task.name]);
+    var i = states.indexOf($scope.params[task]);
     var ni = (i + 1) % states.length;
     var nextState = states[ni];
-    $scope.params[task.name] = nextState;
+    $scope.params[task] = nextState;
   };
 
 });
